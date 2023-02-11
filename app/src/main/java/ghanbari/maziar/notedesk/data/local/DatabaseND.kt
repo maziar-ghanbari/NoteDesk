58209a55c89013e22807b9bb4c60c57ee0fd3e76
maziar <maziar1developer@gmail.com>
2023-02-11 13:12:36 +0330

    publish

--- app/src/main/java/ghanbari/maziar/notedesk/data/local/DatabaseND.kt
+++ app/src/main/java/ghanbari/maziar/notedesk/data/local/DatabaseND.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import ghanbari.maziar.notedesk.data.model.FolderEntity
 import ghanbari.maziar.notedesk.data.model.NoteEntity
 
-@Database(entities = [NoteEntity::class,FolderEntity::class] , version = 1 , exportSchema = false)
+@Database(entities = [NoteEntity::class,FolderEntity::class] , version = 2 , exportSchema = false)
 abstract class DatabaseND : RoomDatabase() {
     abstract fun noteDao() : NoteDao
     abstract fun folderDao() : FolderDao
